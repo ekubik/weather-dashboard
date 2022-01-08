a0f7ed225cd12c814bd98812ded727ecef44797b
Ewa Kubik <ewa.kubik@hotmail.com>
2022-01-09 01:36:41 +1100

    updated html and started work with api

---
+++ Assets/script.js
@@ -0,0 +1,21 @@
+var apiKey = "d958229d63493292b265d62c35a602e7";
+var apiUrl =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  apiKey;
+
+var cityInput = document.getElementById("city-search");
+
+var city = cityInput.value;
+
+var searchBtn = document.getElementById("searchBtn");
+
+function currentWeather() {
+  console.log(city);
+  fetch(apiUrl).then(function (response) {
+    console.log(response);
+    return response.json();
+  });
+}
+searchBtn.addEventListener("click", currentWeather());
