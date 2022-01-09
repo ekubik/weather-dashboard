f6ba4622999f18355157d4410f89372c193cd8fa
Ewa Kubik <ewa.kubik@hotmail.com>
2022-01-09 19:28:46 +1100

    update js. retrieve and display current weather

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,46 @@
-var apiKey = "d958229d63493292b265d62c35a602e7"
-var currentDate = moment().format();
+var apiKey = "d958229d63493292b265d62c35a602e7";
+var dateToday = moment().format("DD/MM/YYYY");
+var currentWeatherEl = document.getElementById("current-weather-container");
 
 var searchBtn = document.getElementById("searchBtn");
 
+
 function currentWeather() {
-event.preventDefault();
+  event.preventDefault();
+  var cityInput = $("input[name='city-search']");
+  var city = cityInput.val();
+  console.log(city);
+  var apiUrl =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=metric&appid=" +
+    apiKey;
+  fetch(apiUrl)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.main.temp);
+      console.log(data.wind.speed);
+      console.log(data.main.humidity);
 
-var apiUrl =
-  "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" +
-  apiKey;
+      var cityName = document.createElement("h2");
+      var currentTemperature = document.createElement("p");
+      var currentWind = document.createElement("p");
+      var currentHumidity = document.createElement("p");
 
-var cityInput = $("input[name='city-search']");
-var city = cityInput.value;
-console.log(city);
-fetch(apiUrl)
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.main.temp);
+      cityName.textContent = city + ",    " + dateToday;
+      currentTemperature.textContent = "Temperature: " + data.main.temp + "°C";
+      currentWind.textContent = "Wind: " + data.wind.speed + " m/s";
+      currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
 
-    var cityName = document.createElement("h2");
-    var currentTemperature = document.createElement("p");
-    var currentWind = document.createElement("p");
-    var currentHumidity = document.createElement("p");
-  })};
+      currentWeatherEl.append(cityName);
+      currentWeatherEl.append(currentTemperature);
+      currentWeatherEl.append(currentWind);
+      currentWeatherEl.append(currentHumidity);
+    });
+}
 
-
-searchBtn.addEventListener("click", currentWeather)
+searchBtn.addEventListener("click", currentWeather);
